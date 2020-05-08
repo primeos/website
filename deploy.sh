5ff12737595cac79e5cb67f20c656febf5aad84e
Michael Weiss <dev.primeos@gmail.com>
2020-05-08 12:26:07 +0200

    deploy.sh: Switch to sshfs and rsync

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
-set -e
+set -eu
 
-echo -e "cd /var/www/html\nput -R public/*" | sftp -b - www.primeos.dev
+SSHFS_DIR=sshfs-public
+
+mkdir "$SSHFS_DIR"
+sshfs www.primeos.dev:/var/www/html/ "$SSHFS_DIR"
+
+rsync -vcrlpi --safe-links --chmod=D0755,F0644 --delete public/ "$SSHFS_DIR"
+# Optional: -t
+
+fusermount3 -u "$SSHFS_DIR"
+rmdir "$SSHFS_DIR"
